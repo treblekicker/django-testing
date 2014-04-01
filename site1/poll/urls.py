73b4705239c7600003fa0090eda22bc0fe80ba9c
Berthold, Robert <rberthol@gannett.com>
2014-04-01 14:46:25 -0400

    initial commit

---
+++ site1/poll/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+from poll.models import Poll
+
+urlpatterns = patterns('',
+    url(r'^$',
+        ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date')[:5],
+            context_object_name='latest_poll_list',
+            template_name='polls/index.html')),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/detail.html')),
+    url(r'^(?P<pk>\d+)/results/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/results.html'),
+        name='poll_results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'poll.views.vote'),
+)
+
+# from django.conf.urls import patterns, url
+# 
+# urlpatterns = patterns('poll.views',
+#     url(r'^$', 'index'),
+#     url(r'^(?P<poll_id>\d+)/$', 'detail'),
+#     url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+#     url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+# )
